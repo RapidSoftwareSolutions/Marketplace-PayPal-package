ae170cab56d9cf4cf11e8002a21473ee5985c17d
triongroup <triongroup@gmail.com>
2016-10-23 12:18:43 +0300

    Update createPayment.php

--- src/routes/createPayment.php
+++ src/routes/createPayment.php
@@ -49,7 +49,7 @@ $app->post('/api/PayPal/createPayment', function ($request, $response, $args) {
     }
     
     
-    if($post_data['args']['sandbox'] == 1) {
+    if(isset($post_data['args']['sandbox']) && $post_data['args']['sandbox'] == 1) {
         $query_str = 'https://api.sandbox.paypal.com/v1/payments/payment';
     } else {
         $query_str = 'https://api.paypal.com/v1/payments/payment';
