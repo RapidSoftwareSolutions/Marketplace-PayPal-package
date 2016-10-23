bb55e56911471f27c949953113e4f2b6a10511d2
triongroup <triongroup@gmail.com>
2016-10-23 12:29:35 +0300

    Update getAgreement.php

--- src/routes/getAgreement.php
+++ src/routes/getAgreement.php
@@ -28,7 +28,7 @@ $app->post('/api/PayPal/getAgreement', function ($request, $response, $args) {
     $headers['Authorization'] = "Bearer " . $post_data['args']['accessToken'];
     $headers['Content-Type'] = 'application/json'; 
     
-    if($post_data['args']['sandbox'] == 1) {
+    if(isset($post_data['args']['sandbox']) && $post_data['args']['sandbox'] == 1) {
         $query_str = 'https://api.sandbox.paypal.com/v1/payments/billing-agreements/'.$post_data['args']['agreementId'];
     } else {
         $query_str = 'https://api.paypal.com/v1/payments/billing-agreements/'.$post_data['args']['agreementId'];
