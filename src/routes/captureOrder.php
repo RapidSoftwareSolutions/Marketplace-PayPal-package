1394b8c5f3b680fbc4fd86ee9fd54b432099dd6c
triongroup <triongroup@gmail.com>
2016-10-23 12:16:57 +0300

    Update captureOrder.php

--- src/routes/captureOrder.php
+++ src/routes/captureOrder.php
@@ -30,7 +30,7 @@ $app->post('/api/PayPal/captureOrder', function ($request, $response, $args) {
     $headers['Content-Type'] = 'application/json'; 
     
     
-    if($post_data['args']['sandbox'] == 1) {
+    if(isset($post_data['args']['sandbox']) && $post_data['args']['sandbox'] == 1) {
         $query_str = 'https://api.sandbox.paypal.com/v1/payments/orders/'.$post_data['args']['orderId'].'/capture';
     } else {
         $query_str = 'https://api.paypal.com/v1/payments/orders/'.$post_data['args']['orderId'].'/capture';
