a425f97efb9f7613132a16633f86dc9937c241c0
triongroup <triongroup@gmail.com>
2016-10-23 12:21:47 +0300

    Update createTokenFromRefreshToken.php

--- src/routes/createTokenFromRefreshToken.php
+++ src/routes/createTokenFromRefreshToken.php
@@ -33,7 +33,7 @@ $app->post('/api/PayPal/createTokenFromRefreshToken', function ($request, $respo
     
     $auth = [$post_data['args']['clientId'], $post_data['args']['secret']];
     
-    if($post_data['args']['sandbox'] == 1) {
+    if(isset($post_data['args']['sandbox']) && $post_data['args']['sandbox'] == 1) {
         $query_str = 'https://api.sandbox.paypal.com/v1/identity/tokenservice';
     } else {
         $query_str = 'https://api.paypal.com/v1/identity/tokenservice';
