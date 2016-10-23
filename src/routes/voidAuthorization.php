2d1a9f5c5594fa2928a2402b8eba7c5be159074a
triongroup <triongroup@gmail.com>
2016-10-23 13:10:01 +0300

    Update voidAuthorization.php

--- src/routes/voidAuthorization.php
+++ src/routes/voidAuthorization.php
@@ -30,7 +30,7 @@ $app->post('/api/PayPal/voidAuthorization', function ($request, $response, $args
     $headers['Content-Type'] = 'application/json'; 
     
     
-    if($post_data['args']['sandbox'] == 1) {
+    if(isset($post_data['args']['sandbox']) && $post_data['args']['sandbox'] == 1) {
         $query_str = 'https://api.sandbox.paypal.com/v1/payments/authorization/'.$post_data['args']['authorizationId'].'/void';
     } else {
         $query_str = 'https://api.paypal.com/v1/payments/authorization/'.$post_data['args']['authorizationId'].'/void';
