73d5ef601086f13db5452473ccb30e4fc5da7143
triongroup <triongroup@gmail.com>
2016-10-23 13:05:36 +0300

    Update simulateWebhookEvent.php

--- src/routes/simulateWebhookEvent.php
+++ src/routes/simulateWebhookEvent.php
@@ -28,7 +28,7 @@ $app->post('/api/PayPal/simulateWebhookEvent', function ($request, $response, $a
     $headers['Authorization'] = "Bearer " . $post_data['args']['accessToken'];
     $headers['Content-Type'] = 'application/json';
     
-    if($post_data['args']['sandbox'] == 1) {
+    if(isset($post_data['args']['sandbox']) && $post_data['args']['sandbox'] == 1) {
         $query_str = 'https://api.sandbox.paypal.com/v1/notifications/simulate-event';
     } else {
         $query_str = 'https://api.paypal.com/v1/notifications/simulate-event';
