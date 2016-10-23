55b3bbcb4e3c6c1bc68ea36f8a4857f14cfed1c5
triongroup <triongroup@gmail.com>
2016-10-23 12:54:02 +0300

    Update getSale.php

--- src/routes/getSale.php
+++ src/routes/getSale.php
@@ -30,7 +30,7 @@ $app->post('/api/PayPal/getSale', function ($request, $response, $args) {
     $headers['Content-Type'] = 'application/json'; 
     
     
-    if($post_data['args']['sandbox'] == 1) {
+    if(isset($post_data['args']['sandbox']) && $post_data['args']['sandbox'] == 1) {
         $query_str = 'https://api.sandbox.paypal.com/v1/payments/sale/'.$post_data['args']['saleId'];
     } else {
         $query_str = 'https://api.paypal.com/v1/payments/sale/'.$post_data['args']['saleId'];
