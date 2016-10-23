487fcb74920a2ffdc1dc83d27dfba573746e93e6
triongroup <triongroup@gmail.com>
2016-10-23 12:52:03 +0300

    Update getPlanList.php

--- src/routes/getPlanList.php
+++ src/routes/getPlanList.php
@@ -25,7 +25,7 @@ $app->post('/api/PayPal/getPlanList', function ($request, $response, $args) {
     $headers['Authorization'] = "Bearer " . $post_data['args']['accessToken'];
     $headers['Content-Type'] = 'application/json'; 
     
-    if($post_data['args']['sandbox'] == 1) {
+    if(isset($post_data['args']['sandbox']) && $post_data['args']['sandbox'] == 1) {
         $query_str = 'https://api.sandbox.paypal.com/v1/payments/billing-plans';
     } else {
         $query_str = 'https://api.paypal.com/v1/payments/billing-plans';
